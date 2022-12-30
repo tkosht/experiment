a64cd7c519782986868fb67fd08a3e558bd4ea90
tkosht <takehito.oshita.business@gmail.com>
2022-12-30 19:09:49 +0900

    mod: simple implement

--- backend/app/component/models/pipeline.py
+++ backend/app/component/models/pipeline.py
@@ -1,13 +1,37 @@
-from sklearn.pipeline import Pipeline as PipelineSklearn
+from typing_extensions import Self
 
 from .model import Labeller, Model, Tensor
 
 
-class Pipeline(Model, PipelineSklearn):
-    def __init__(self, steps=list[tuple(Model, Labeller)]) -> None:
+class Pipeline(Model):
+    def __init__(self, steps=list[tuple[Model, Labeller]], name="pipeline") -> None:
         super().__init__()
 
-        self.steps = steps
+        self.name = name
+        self.steps: list[tuple[Model, Labeller]] = steps
+        self._initialize()
+
+    def _initialize(self) -> Self:
+        for idx, (m, _l) in enumerate(self.steps):
+            setattr(self, f"{self.name}_layer_{idx:03d}", m)
+        return self
 
     def forward(self, X: Tensor) -> Tensor:
-        raise NotImplementedError(f"{type(self).__name__}.forward()")
+        y = None  # output
+
+        h = X
+        for model, labeller in self.steps:
+            # apply model
+            y = model(h)
+
+            # NOTE: just a simple implement
+            if labeller is not None:
+                assert hasattr(model, "loss")
+                t = labeller(y)
+                loss: Tensor = model.loss(y, t)
+                print(f"loss={loss.data}")
+
+            # update variable `h` as hidden output tensor
+            h = y
+
+        return y
--- backend/app/executable/train_wordvector.py
+++ backend/app/executable/train_wordvector.py
@@ -1,6 +1,8 @@
 from app.component.models.pipeline import Pipeline
-from app.domain.models.tokenizer import JpTokenizerJanome
+from app.domain.models.tokenizer import TokenizerWord
 
 if __name__ == "__main__":
-    pipe = Pipeline(steps=[JpTokenizerJanome(), None])
-    pass
+    pipe = Pipeline(steps=[(TokenizerWord(), None)])
+    X = [["すもももももももものうち"]]
+    y = pipe(X)
+    print(y)
