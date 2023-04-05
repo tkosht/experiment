0854d4769c3687dd7fffbe3b559d5093eb3094cf
tkosht <takehito.oshita.business@gmail.com>
2023-04-05 19:17:55 +0900

    mod: off the model.eval()

--- backend/app/general/models/trainer.py
+++ backend/app/general/models/trainer.py
@@ -140,7 +140,7 @@ class TrainerBertClassifier(TrainerBase):
         return self
 
     def do_eval(self, epoch=None, step=None) -> Self:
-        self.model.eval()
+        # self.model.eval()
         total_loss = []
         for bch_idx, bch in enumerate(tqdm(self.validloader, desc="validloader")):
             inputs, t = self._t(bch)
