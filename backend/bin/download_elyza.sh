501f4e761c17841ea9b282df83426a93deb4dd3c
tkosht <takehito.oshita.business@gmail.com>
2024-01-08 13:24:08 +0900

    mod: updated download script

--- backend/bin/download_elyza.sh
+++ backend/bin/download_elyza.sh
@@ -2,6 +2,6 @@
 d=$(cd $(dirname $0) && pwd)
 cd $d/../data
 
-# wget https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-7b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-7b-fast-instruct-q8_0.gguf
-# wget https://huggingface.co/mmnga/ELYZA-japanese-CodeLlama-7b-instruct-gguf/resolve/main/ELYZA-japanese-CodeLlama-7b-instruct-q8_0.gguf
+wget https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-7b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-7b-fast-instruct-q8_0.gguf
+wget https://huggingface.co/mmnga/ELYZA-japanese-CodeLlama-7b-instruct-gguf/resolve/main/ELYZA-japanese-CodeLlama-7b-instruct-q8_0.gguf
 wget https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-13b-fast-instruct-gguf/resolve/main/ELYZA-japanese-Llama-2-13b-fast-instruct-q8_0.gguf
